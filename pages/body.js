c9aa92f57219ec0e758770bb985586caa8cbbe8a
Chamith <chameeefdo@gmail.com>
2022-02-01 11:48:06 +0530

    ready-for-deploy 5

--- pages/body.js
+++ pages/body.js
@@ -2,23 +2,21 @@ import React from 'react';
 
 export default function Body({ posts  }) {
 
+
    
 
-    if(posts){
-      let postMap = posts.map(post => (
+    return (
+    
+        <ul className="list-group mb-3">
+            
+          {posts.map(post => (
             
             <li key={post.id} className="list-group-item">
               {post.id} -- {post.title}
             </li>
             
             
-          ));
-    }
-
-    return (
-    
-        <ul className="list-group mb-3">
-          {postMap}
+          ))}
         </ul>
       );
 
